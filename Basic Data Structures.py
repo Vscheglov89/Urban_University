e2d92866bf3d1da178d1d1ebe35ea47c31695879
Vscheglov89 <vasiliy_a_scheglov@mail.ru>
2024-07-16 15:26:03 +0300

    Add files via upload

---
+++ Basic Data Structures.py
@@ -0,0 +1,14 @@
+ # 1st program
+print(9**0.5*5)
+# 2nd program
+print(9.99>9.98 and 1000!=1001)
+# 3rd program
+# 1234 , 5678
+print((1234 % 1000 // 10) + (5678 % 1000 // 10) )
+# 4th program
+# 13.42 , 42.13
+a = 13.42
+b = 42.13
+ai = int(13.42)
+bi = int(42.13)
+print((round((a - ai) * 100)) == bi) and print((round((b - bi) * 100)) == ai)
