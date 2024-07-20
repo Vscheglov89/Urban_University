3498018697bd39064b8d40b87841e6005a2437b4
Vscheglov89 <vasiliy_a_scheglov@mail.ru>
2024-07-20 12:36:24 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+# 2
+immutable_var = 1, 2, 3, 4, 'Copy'
+print(immutable_var)
+# 4
+mutable_list = [4, 3, 2, 1, 'Copy']
+print(mutable_list)
+mutable_list[0] = 9
+print(mutable_list)
+# 3
+immutable_var[0] = 3
+
+
