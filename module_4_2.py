30055e67f1c0588fabfe27c534bb6d9ca6d70b84
Vscheglov89 <vasiliy_a_scheglov@mail.ru>
2024-09-22 17:26:35 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,14 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+        inner_function() #не работает
+
+inner_function() # Ошибка: 'inner_function' is not defined. Did you mean: 'test_function'?
+# Возникла из-за разницы пространства имен, написана внутри inner_function(),
+
+
+
+
+
+
