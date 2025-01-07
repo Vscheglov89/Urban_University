a8386410a246c7e410953c23901db06145018d0f
Vscheglov89 <vasiliy_a_scheglov@mail.ru>
2025-01-07 16:56:57 +0300

    Add files via upload

--- module_3_5.py
+++ module_3_5.py
@@ -1,12 +1,20 @@
-
-def get_multiplied_digits(number):
+def get_multiplied_digits(number): # Преобразуем число в строку для обработки
     str_number = str(number)
-    first = int(str_number[0])
 
-    if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
-    else:
-        return first
+    if number == 0: # Если число равно 0, возвращаем 0
+        return 0
+
+    product = 1 # Инициализируем произведение
+
+    for digit in str_number:
+        digit_value = int(digit)
+        if digit_value != 0:  # Игнорируем нули
+            product *= digit_value
+
+    return product
+
 
 result = get_multiplied_digits(40203)
 print(result)
+result2 = get_multiplied_digits(402030)
+print(result2)
