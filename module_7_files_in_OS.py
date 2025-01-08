c77488ffa26c08440e22a02fdf879079ba6d6931
Vscheglov89 <vasiliy_a_scheglov@mail.ru>
2025-01-08 15:40:07 +0300

    Add files via upload

---
+++ module_7_files_in_OS.py
@@ -0,0 +1,17 @@
+import os
+import time
+
+directory = r'C:\Users\Щегловы\PycharmProjects\pythonProject'
+
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        filepath = os.path.join(root, file)  # Полный путь к файлу.
+        filetime = os.path.getmtime(filepath)  # Время последнего изменения файла.
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        filesize = os.path.getsize(filepath)  # Размер файла.
+        parent_dir = os.path.dirname(filepath)  # Родительская директория файла.
+        print(f'Обнаружен файл: {file},'
+              f' Путь: {filepath},'
+              f' Размер: {filesize} байт,'
+              f' Время изменения: {formatted_time},'
+              f' Родительская директория: {parent_dir}')
